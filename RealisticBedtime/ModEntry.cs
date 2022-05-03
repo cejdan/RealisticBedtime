d8af0e36c9559fac458793ff5cd27ad760f7aff5
Nic Cejda <59983880+cejdan@users.noreply.github.com>
2022-05-03 09:56:42 -0500

    Updated the bedtimes that result in penalties

--- RealisticBedtime/ModEntry.cs
+++ RealisticBedtime/ModEntry.cs
@@ -44,7 +44,6 @@ namespace RealisticBedtime
         ** Private variables
         ******/
 
-        //private float currentStamina;
         private int timeDayEnded = 600;
         
         /*********
@@ -54,9 +53,9 @@ namespace RealisticBedtime
         {
             // Our task in this event is to reload the previous day end time.
 
-            ModData model = new ModData(600); // this will call the model constructor, sets the previous_bedtime to 600 and bool to false.
+            ModData model = new ModData(600); // this will call the model constructor, sets the previous_bedtime to 600.
 
-            // Reads the saveed data. If this is the 1st day, previous_bedtime is set to 0 and bool is false. Otherwise, should be correct.
+            // Reads the saveed data. If this is the 1st day, previous_bedtime is set to 600. Otherwise, should be correct.
             var previousData = this.Helper.Data.ReadSaveData<ModData>("previous_bedtime");
 
             if (previousData == null) // if we didn't read any data, then the data doesn't exist! We need to make some data to read:
@@ -70,84 +69,83 @@ namespace RealisticBedtime
             // It works! The game now remembers your previous bedtime!
 
             // NOTE: MULTIPLAYER IS UNTESTED BUT LIKELY ONLY WORKS FOR THE MAIN PLAYER!
-            // In multiplayer other farmers probably will start every fresh play session with energized.
+            // In multiplayer other farmers probably will either start every fresh play session with energized, or be totally unaffected by the mod.
         
         }
 
 
         private void whenTimeChange(object sender, TimeChangedEventArgs e)
         {
-            //Monitor.Log($"Time was updated! Old time was: {e.OldTime}. New time is currently: {e.NewTime}", LogLevel.Debug)
-            
-            if (e.NewTime == 610) {
+            if (e.NewTime == 2210) {
                 Game1.addHUDMessage(new HUDMessage("You are feeling tired...", 3));
             }
-            else if (e.NewTime == 700)
+            else if (e.NewTime == 2410)
             {
                 Game1.addHUDMessage(new HUDMessage("You are feeling very tired...", 3));
             }
-            else if (e.NewTime == 800)
+            else if (e.NewTime == 2510)
             {
                 Game1.addHUDMessage(new HUDMessage("You are feeling exhausted!", 3));
-            }
-
-            //{
-            //    Monitor.Log($"It is currently past 6:10!", LogLevel.Debug);
-            //}
-
-            //currentStamina = currentStamina + 5;
-            //Game1.player.Stamina = currentStamina;
-    
+            }  
         }
 
         private void whenDayStarted(object sender, DayStartedEventArgs e)
         {
             // Define new buffs that can be applied at the beginning of the day.
-            var energized = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 15, "speed", "energized!")
+            var energized = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 15, "speed", "Energized!")
             {
                 which = 928880,
                 millisecondsDuration = 900000
             };
 
-            var tired = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 15, "speed", "tired")
+            var tired = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 15, "speed", "Tired")
             {
                 which = 928883,
                 millisecondsDuration = 900000
             };
 
-            var veryTired = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, -2, 0, 0, 15, "speed", "very tired")
+            var veryTired = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, -2, 0, 0, 15, "speed", "Very Tired")
             {
                 which = 928883,
                 millisecondsDuration = 900000
             };
 
-            var exhausted = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, -3, 0, 0, 15, "speed", "exhausted!")
+            var exhausted = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, -3, 0, 0, 15, "speed", "Exhausted!")
+            {
+                which = 928883,
+                millisecondsDuration = 900000
+            };
+            
+            var nonFunctional = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, -10, 0, 0, 15, "speed", "Non-functional...")
             {
                 which = 928883,
                 millisecondsDuration = 900000
             };
 
-
-            if (timeDayEnded == 600)
+            if (timeDayEnded <= 2200) // Sleep before or at 10:00 pm, you are energized!
             {
                 Game1.buffsDisplay.addOtherBuff(energized);
             }
 
-            else if (timeDayEnded > 600 && timeDayEnded <= 700)
+            else if (timeDayEnded > 2200 && timeDayEnded <= 2400) // Between 10:10pm and 12:00 am, you are tired!
             {
                 Game1.player.Stamina = (Game1.player.Stamina / 2);
                 Game1.buffsDisplay.addOtherBuff(tired);
             }
-            else if (timeDayEnded > 700 && timeDayEnded <= 800)
+            else if (timeDayEnded > 2400 && timeDayEnded <= 2500) // Between 12:10 and 1:00 am, you are very tired!
             {
                 Game1.player.Stamina = (Game1.player.Stamina / 3);
                 Game1.buffsDisplay.addOtherBuff(veryTired);
             }
-            else
+            else if (timeDayEnded > 2500 && timeDayEnded <= 2550) // Between 1:10 am and 1:50 am, you are exhausted!
             {
                 Game1.player.Stamina = (Game1.player.Stamina / 4);
                 Game1.buffsDisplay.addOtherBuff(exhausted);
             }
+            else // Pass out at 2am, you are non-functional!
+            {
+                Game1.player.Stamina = (Game1.player.Stamina * 0);
+                Game1.buffsDisplay.addOtherBuff(nonFunctional);
         } 
 
         private void whenDayEnded(object sender, DayEndingEventArgs e)
